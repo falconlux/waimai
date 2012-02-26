297670a4b63108817a2ac68faeb88387b8c9a11e
Lu Xiang <falconlux@falcon.local>
2012-02-26 14:23:48 +0800

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waimai::Application.config.secret_token = 'dc0dfa1192cf15fc2381dd936164465211e05955ffca3dfd313113c47be38da7ac4ffa78ae06abc724fa77fc3bc6d36232e7312bc9321ea888f82dc07be02af9'
